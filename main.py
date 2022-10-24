1cfe8e8b926dce034193fafc385fa1d6f8191dba
QuentinHelion <helion.quentin@gmail.com>
2022-10-24 12:31:01 +0200

    update .env var

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import tweepy
 from decouple import config
 
-
 # Get .env variable
 API_KEY = config('API_KEY')
 API_KEY_SECRET = config('API_KEY_SECRET')
@@ -10,13 +9,16 @@ ACCESS_TOKEN_SECRET = config('ACCESS_TOKEN_SECRET')
 
 
 # Authenticate to Twitter
-auth = tweepy.OAuthHandler(API_KEY,API_KEY_SECRET)
-auth.set_access_token(ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
+# auth = tweepy.OAuthHandler(API_KEY,API_KEY_SECRET)
+#
+# auth.set_access_token(ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
+#
+# api = tweepy.API(auth)
+#
+# try:
+#     api.verify_credentials()
+#     print("Authentication OK")
+# except:
+#     print("Error during authentication")
 
-api = tweepy.API(auth)
-
-try:
-    api.verify_credentials()
-    print("Authentication OK")
-except:
-    print("Error during authentication")
+print("end")
